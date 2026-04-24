acd5497d8371860c903a65bf7ddf31fe6646df0b
Axl Nunez <axltico12@gmail.com>
2026-04-24 01:01:53 -0400

    Refactored server discovery logic

--- lib/auth/services/server_discovery_service.dart
+++ lib/auth/services/server_discovery_service.dart
@@ -22,69 +22,62 @@ class ServerDiscoveryService {
   static const _discoveryPort = 7359;
   static const _jellyfinMessage = 'who is JellyfinServer?';
   static const _embyMessage = 'who is EmbyServer?';
-  static const _receiveTimeoutMs = 5000;
-  static const _discoveryRounds = 3;
-  static const _roundDelay = Duration(milliseconds: 1500);
+  static const _listenDuration = Duration(milliseconds: 2500);
+  static const _rebroadcastInterval = Duration(milliseconds: 800);
 
   Stream<DiscoveredServer> discoverLocalServers() async* {
+    final controller = StreamController<DiscoveredServer>();
     final seen = <String>{};
-
-    for (var round = 0; round < _discoveryRounds; round++) {
-      try {
-        final servers = await _sendDiscoveryBroadcast();
-        for (final server in servers) {
-          if (seen.add(server.id)) {
-            yield server;
-          }
-        }
-      } catch (_) {}
-
-      if (round < _discoveryRounds - 1) {
-        await Future.delayed(_roundDelay);
-      }
-    }
-  }
-
-  Future<List<DiscoveredServer>> _sendDiscoveryBroadcast() async {
-    final results = <DiscoveredServer>[];
     RawDatagramSocket? socket;
+    Timer? rebroadcastTimer;
+    Timer? stopTimer;
 
     try {
       socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
       socket.broadcastEnabled = true;
 
       final broadcastAddresses = await _getBroadcastAddresses();
-
       final jellyfinData = utf8.encode(_jellyfinMessage);
       final embyData = utf8.encode(_embyMessage);
-      for (final addr in broadcastAddresses) {
-        socket.send(jellyfinData, addr, _discoveryPort);
-        socket.send(embyData, addr, _discoveryPort);
+
+      void sendBroadcasts() {
+        for (final addr in broadcastAddresses) {
+          try {
+            socket!.send(jellyfinData, addr, _discoveryPort);
+            socket.send(embyData, addr, _discoveryPort);
+          } catch (_) {}
+        }
       }
 
-      final completer = Completer<List<DiscoveredServer>>();
-      final timer = Timer(Duration(milliseconds: _receiveTimeoutMs), () {
-        if (!completer.isCompleted) completer.complete(results);
-      });
-
       socket.listen((event) {
-        if (event == RawSocketEvent.read) {
-          final datagram = socket!.receive();
-          if (datagram != null) {
-            final response = utf8.decode(datagram.data);
-            final server = _parseResponse(response);
-            if (server != null) results.add(server);
-          }
+        if (event != RawSocketEvent.read) return;
+        final datagram = socket!.receive();
+        if (datagram == null) return;
+        final response = utf8.decode(datagram.data);
+        final server = _parseResponse(response);
+        if (server == null) return;
+        if (seen.add(server.id)) {
+          controller.add(server);
         }
       });
 
-      await completer.future;
-      timer.cancel();
-    } finally {
-      socket?.close();
-    }
+      sendBroadcasts();
+      rebroadcastTimer = Timer.periodic(_rebroadcastInterval, (_) {
+        sendBroadcasts();
+      });
+      stopTimer = Timer(_listenDuration, () {
+        if (!controller.isClosed) controller.close();
+      });
 
-    return results;
+      yield* controller.stream;
+    } catch (_) {
+      if (!controller.isClosed) await controller.close();
+    } finally {
+      rebroadcastTimer?.cancel();
+      stopTimer?.cancel();
+      socket?.close();
+      if (!controller.isClosed) await controller.close();
+    }
   }
 
   DiscoveredServer? _parseResponse(String response) {
